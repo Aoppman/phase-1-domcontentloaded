d2266e9cc50e125069b3709cf14eca7a14623af7
Andrew <a.oppman@gmail.com>
2023-06-11 13:41:04 -0400

    code added to index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", changeText);
+
+//console.log("This will appear before DOM content loaded is triggered!")
+
+
+const text = document.getElementById('text')
+
+function changeText() {
+    text.textContent = "This is really cool!"
+}
